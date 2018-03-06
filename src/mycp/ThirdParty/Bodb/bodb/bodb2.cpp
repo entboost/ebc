447d8e8fb8e898ece978a89ed1de1c6ccad4c53f
akee_yang@kingdee.com <akee_yang@kingdee.com>
2018-03-06 15:06:12 +0800

    整理 bodb 源码。

--- src/mycp/ThirdParty/Bodb/bodb/bodb2.cpp
+++ src/mycp/ThirdParty/Bodb/bodb/bodb2.cpp
@@ -1302,10 +1302,10 @@ namespace bo
 
 	const std::string & replace(std::string & strSource, const std::string & strFind, const std::string &strReplace)
 	{
-		std::string::size_type pos=0;
-		std::string::size_type findlen=strFind.size();
-		std::string::size_type replacelen=strReplace.size();
-		while ((pos=strSource.find(strFind, pos)) != std::string::npos)
+		tstring::size_type pos=0;
+		tstring::size_type findlen=strFind.size();
+		tstring::size_type replacelen=strReplace.size();
+		while ((pos=strSource.find(strFind, pos)) != tstring::npos)
 		{
 			strSource.replace(pos, findlen, strReplace);
 			pos += replacelen;
