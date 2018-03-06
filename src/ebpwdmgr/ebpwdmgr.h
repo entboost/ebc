8dd5584794fda9bcca09cdd6b3a45b88c2d1fef9
akee_yang@kingdee.com <akee_yang@kingdee.com>
2018-03-06 09:03:19 +0800

    添加修改系统管理员密码工程ebpwdmgr源码。

---
+++ src/ebpwdmgr/ebpwdmgr.h
@@ -0,0 +1,32 @@
+
+// ebpwdmgr.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CebpwdmgrApp:
+// See ebpwdmgr.cpp for the implementation of this class
+//
+
+class CebpwdmgrApp : public CWinAppEx
+{
+public:
+	CebpwdmgrApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CebpwdmgrApp theApp;
---
+++ src/ebpwdmgr/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ebpwdmgr.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_EBPWDMGR_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_TEST_CONNECT            1000
+#define IDC_BTN_CHANGE_PWD              1001
+#define IDC_STATIC_DB_BINPATH           1002
+#define IDC_EDIT_DB_IP                  1006
+#define IDC_EDIT_DB_PORT                1007
+#define IDC_EDIT_DB_NAME                1008
+#define IDC_EDIT_DB_ACCOUNT             1009
+#define IDC_EDIT_DB_SECURE              1010
+#define IDC_EDIT_ADMIN_SECURE           1017
+#define IDC_EDIT_NEW_SECURE             1017
+#define IDC_BUTTON_SET_BIN_PATH         1022
+#define IDC_COMBO_DBTYPE                1025
+#define IDC_COMBO_PWDMOE                1026
+#define IDC_COMBO_PWDMODE               1026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
